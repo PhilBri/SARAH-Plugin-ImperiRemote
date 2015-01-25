2702cac4a64815fb98dbc874db9ff8a5d9fba7e3
Phil Bri <ph.brioude@gmail.com>
2015-01-25 18:40:17 +0100

    V0.1

---
+++ ImperiRemote.js
@@ -0,0 +1,43 @@
+/*_______________________________________________________
+|                 ImperiRemote v0.1                      |
+|                                                        |
+| Authors : Arnaud Peter & Phil Bri ( 01/2015 )          |
+| Description :                                          |
+|    Imperihome Plugin for SARAH project                 |
+|    (See http://encausse.wordpress.com/s-a-r-a-h/)      |
+|________________________________________________________|
+*/
+
+exports.action = function ( data , callback , config , SARAH ) {
+
+	var	config = cfg.modules.ImperiRemote;
+
+	if ( !config.IP || !config.Port) {
+		console.log ("\nImperiRemote [Erreur] => IP ou Port non paramétré !");
+		return callback ({ 'tts' : 'Erreur de configuration de l\'I P ou du port' });
+	}
+
+	var http = require('http');
+	var options = {
+    	host: config.IP,
+    	port: config.Port,
+    	path: '/api/rest/dashboard/gotopage?' + data.cmd;
+  	};
+
+	var req = http.get ( options, function ( response ) {
+  		var res_data = '';
+
+  		response.on ( 'data', function ( chunk ) {
+    		res_data += chunk;
+  		});
+
+  		response.on ( 'end', function () {
+  			console.log ( '\nImperiRemote [OK] => Commande : ' + data.cmd + ' Retour : ' + res_data );
+  			callback ({ 'tts' : data.ttsAction });
+  		});
+	});
+
+	req.on( 'error', function ( e ) {
+		return console.log ( "ImperiRemote => Erreur : " + e.message );
+	});
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# ImperiRemote
+
+Plugin for S.A.R.A.H. project by JP Encausse
+http://blog.encausse.net/s-a-r-a-h/
+
+
+## ImperiRemote Setup
+
+Téléchargez le plugin ImperiRemote sur le MarketPlace de SARAH
+http://marketplace.sarah.encausse.net/store
+
+Démarrez SARAH et lancez l'interface Web du client http://127.0.0.1:8080/home
+
+Dans le portlet ImperiRemote, renseignez les champs suivants :
+- IP       = 'IP de ImperiHome'.
+- Port     = 'Port de ImperiHome'.
+
+
+## ImperiRemote Example
+
+- "SARAH affiche ImperiHome".
